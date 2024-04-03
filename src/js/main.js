47c80824f3e65a622b8110420d71b57caf22b1a2
Juray <juraypodusovskyy@gmail.com>
2024-04-04 00:38:41 +0300

    tset commit

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,10 @@ import sliders from './modules/sliders';
 window.addEventListener('DOMContentLoaded',()=>{
   const modalState = {usedFlag:false}; 
 
-  modal('.button-design','.popup-close','.popup-design',false,modalState);
-  modal('.fixed-gift','.popup-gift popup-close','.popup-gift',true,modalState);
-  showWhenScrolling('.fixed-gift','.footer',modalState);
-  showByTime('.button-design',60000,modalState);
+  modal( '.button-design', '.popup-close', '.popup-design', false, modalState);
+  modal( '.fixed-gift', '.popup-gift popup-close', '.popup-gift', true, modalState);
+  showWhenScrolling('.fixed-gift','.footer', modalState);
+  showByTime( '.button-design', 60000, modalState );
 
   sliders('.main-slider-item','vertical');
   sliders('.feedback-slider-item','','.main-prev-btn','.main-next-btn');
