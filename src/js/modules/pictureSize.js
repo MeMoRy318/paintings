f17cd5f2ce242c3d0aeb8542faa962a7892e2c83
Juray <juraypodusovskyy@gmail.com>
2024-04-09 16:16:55 +0300

    Refactoring pictureSize

--- src/js/modules/pictureSize.js
+++ src/js/modules/pictureSize.js
@@ -1,37 +1,35 @@
 const pictureSize = () => {
   const sizesBlocks = document.querySelectorAll('.sizes-block');
 
+  const handleMouseOver = (block) => {
+    for (let child of block.children) {
+      if (child.tagName === 'IMG') {
+        const childAttribute = child.getAttribute('src');
+        const upChildAttribute = childAttribute.replace('.png', '-1.png');
+        child.setAttribute('src', upChildAttribute);
+        child.classList.add('animated', 'bounceIn');
+      } else if (!child.classList.contains('sizes-hit')) {
+        child.style.display = 'none';
+      }
+    }
+  };
+
+  const handleMouseOut = (block) => {
+    for (let child of block.children) {
+      if (child.tagName === 'IMG') {
+        const childAttribute = child.getAttribute('src');
+        const upChildAttribute = childAttribute.replace('-1.png', '.png');
+        child.setAttribute('src', upChildAttribute);
+        child.classList.remove('animated', 'bounceIn');
+      } else if (!child.classList.contains('sizes-hit')) {
+        child.style.display = 'block';
+      }
+    }
+  };
+
   sizesBlocks.forEach(sizesBlock => {
-    const handleMouseOver = () => {
-      for (let child of sizesBlock.children) {
-        if (child.tagName === 'IMG') {
-          const childAttribute = child.getAttribute('src');
-          const upChildAttribute = childAttribute.replace('.png', '-1.png');
-          child.setAttribute('src', upChildAttribute);
-
-          child.classList.add('animated', 'bounceIn');
-        } else if (!child.classList.contains('sizes-hit')) {
-          child.style.display = 'none';
-        }
-      }
-    };
-
-    const handleMouseOut = () => {
-      for (let child of sizesBlock.children) {
-        if (child.tagName === 'IMG') {
-          const childAttribute = child.getAttribute('src');
-          const upChildAttribute = childAttribute.replace('-1.png', '.png');
-          child.setAttribute('src', upChildAttribute);
-
-          child.classList.remove('animated', 'bounceIn');
-        } else if (!child.classList.contains('sizes-hit')) {
-          child.style.display = 'block';
-        }
-      }
-    };
-
-    sizesBlock.addEventListener('mouseover', handleMouseOver);
-    sizesBlock.addEventListener('mouseout', handleMouseOut);
+    sizesBlock.addEventListener('mouseover', ()=> handleMouseOver(sizesBlock));
+    sizesBlock.addEventListener('mouseout', ()=> handleMouseOut(sizesBlock));
   });
 };
 
